0d9ab7420ff469fe13ca1b7a99f8204a82b64f42
wada kei <wadakei1210@gmail.com>
2020-12-02 22:07:12 +0900

    add declare svg etc...

--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import PizzaCSS from "./Pizza.module.css";
+import { useSetState } from "./AppState";
 
 interface Pizza {
   id: number;
@@ -6,17 +8,48 @@ interface Pizza {
   description: string;
   price: number;
 }
-interface Props {
+interface Prop {
   pizza: Pizza;
 }
 
-const Pizza: React.FC<Props> = ({ pizza }) => {
+const Pizza: React.FC<Prop> = ({ pizza }) => {
+  const setState = useSetState();
+  const handleAddToCartClick = () => {
+    setState((state) => {
+      const itemExits = state.cart.items.find((item) => item.id === pizza.id);
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          items: itemExits
+            ? state.cart.items.map((item) => {
+                if (item.id === pizza.id) {
+                  return { ...item, quantity: item.quantity + 1 };
+                }
+                return item;
+              })
+            : [
+                ...state.cart.items,
+                {
+                  id: pizza.id,
+                  name: pizza.name,
+                  price: pizza.price,
+                  quantity: 1,
+                },
+              ],
+        },
+      };
+    });
+  };
   return (
-    <li>
+    <ul className={PizzaCSS.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
-    </li>
+      <button type="button" onClick={handleAddToCartClick}>
+        Add to Cart
+      </button>
+    </ul>
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   entry: "./src/index.tsx",
   devtool: "eval-source-map",
@@ -12,6 +14,24 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { modules: true } },//cssfileについてtypescript側に書いたCSSファイルを読み込ませる設定
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: "@svgr/webpack",
+        options: {
+          svgoConfig: {
+            plugins: {
+              removeViewBox: false,//SVGファイルの大きさ、トリミングについて
+            },
+          },
+        },
+      },
     ],
   },
   plugins: [
@@ -19,5 +39,6 @@ module.exports = {
       template: "./src/index.html",
       filename: "index.html",
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
